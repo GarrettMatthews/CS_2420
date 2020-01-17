af52e0d69c36ed9102d6b4de1cbf1432719c73c9
GarrettMatthews <garrettemathews@gmail.com>
2020-01-16 18:39:13 -0700

    updating project 1

--- Project_1/gm_search.py
+++ Project_1/gm_search.py
@@ -4,56 +4,78 @@ from recursioncounter import RecursionCounter
 import time
 import math
 
+
 def linear_search(lyst, target):
     """Function to run a linear search through a lyst, to find a target"""
-    #if not isinstance(lyst, int):
-    #    raise ValueError("List must be a list of integers")
+    if not all(isinstance(x, int) for x in lyst):
+        raise ValueError("Error. Must submit a list of integers only")
+    if not isinstance(target, int):
+        raise ValueError("Error. Target must be an integer")
     return target in lyst
 
-def recursive_binary_search(lyst, target, low_index = 0, high_index = None):
+def recursive_binary_search(lyst,target):
+    """Function to call the recursive binary search function"""
+    if not all(isinstance(x, int) for x in lyst):
+        raise ValueError("Error. Must submit a list of integers only")
+    if not isinstance(target, int):
+        raise ValueError("Error. Target must be an integer")
+    return recursive_binary_search_helper(lyst,target)
+
+
+def recursive_binary_search_helper(lyst, target, low_index = 0, high_index = None):
     """Function to recursively search through a lyst by comparing its value with the midpoint"""
-    #RecursionCounter()
-    #if not isinstance(lyst, int):
-    #    raise ValueError("List must be a list of integers")
+    RecursionCounter()
     if high_index == None:
-        high_index = len(lyst) -1
-    midpoint = (lyst[low_index] + lyst[high_index]) // 2
+        high_index = len(lyst) - 1
+    midpoint = (low_index + high_index) // 2
     if low_index > high_index:
         return False
-    elif midpoint == target:
+    elif lyst[low_index] == target:
         return True
-    elif midpoint < target:
-        return recursive_binary_search(lyst,target, midpoint - 1, high_index)
-    elif midpoint > target:
-        return recursive_binary_search(lyst,target, low_index, midpoint + 1)
+    elif lyst[high_index] == target:
+        return True
+    elif low_index + 2 == high_index:
+        return False
+    elif lyst[midpoint] == target:
+        return True
+    elif lyst[midpoint] < target:
+        return recursive_binary_search(lyst, target, midpoint - 1, high_index)
+    elif lyst[midpoint] > target:
+        return recursive_binary_search(lyst, target, low_index, midpoint + 1)
     else:
         return False
 
-def jump_search(lyst,target, search_index = 0):
-    #if not isinstance(lyst, int):
-    #    raise ValueError("List must be a list of integers")
+
+def jump_search(lyst,target, start = 0):
+    if not all(isinstance(x, int) for x in lyst):
+        raise ValueError("Error. Must submit a list of integers only")
+    if not isinstance(target, int):
+        raise ValueError("Error. Target must be an integer")
     step = int(math.sqrt(len(lyst)))
-    while search_index < len(lyst):
-        if lyst[search_index] == target:
-            return True
-        else:
-            return jump_search(lyst,target, min((search_index + step),(len(lyst)-1)))
+    end = start + step
+    temp = lyst[start:min(len(lyst),end)]
+    if target in temp:
+        return True
+    else:
+        return jump_search(lyst,target, start + step)
     return False
+
 
 def main():
     """Main function to run the progams"""
     print("Creating sorted array of 1000000")
     random.seed(0)
-    #lyst = random.sample(range(100), k=10)
-    lyst = list(range(100))
+    lyst = random.sample(range(100000000000), k=100000)
+    #lyst = list(range(100))
     lyst.sort()
     print("Finished creating sorted array of 1000000")
-    #lin = linear_search(lyst, target = 10)
-    #rec = recursive_binary_search(lyst, 10)
-    jum = jump_search(lyst, 10)
-    #print("Linear search results: ",lin)
-    #print("Recursive search results: ", rec)
+    lin = linear_search(lyst,lyst[0])
+    rec = recursive_binary_search(lyst, lyst[-1])
+    jum = jump_search(lyst, lyst[5])
+    print("Linear search results: ",lin)
+    print("Recursive search results: ", rec)
     print("Jump search results: ", jum)
+
 
 
 if __name__ == "__main__":
